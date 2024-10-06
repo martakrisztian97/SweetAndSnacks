70aebaf71e11d3020272693d438c28c75937a8d7
Márta Krisztián <martakrisztian97@gmail.com>
2024-10-06 15:35:29 +0200

    -u PDF added

---
+++ js/szures.js
@@ -0,0 +1,5 @@
+function szures() {
+    var kategoria = document.getElementById('szures').value;
+    window.open("https://martakrisztian97.github.io/SweetAndSnacks/preorder.html#".kategoria);
+    document.getElementById('szuresBtn').text = kategoria;
+}
