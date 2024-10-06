5e2506aa53c0fcaa2f998122eece2e3f1f2cb24a
Márta Krisztián <martakrisztian97@gmail.com>
2024-10-06 17:42:20 +0200

    u- add category

--- js/szures.js
+++
@@ -1,5 +0,0 @@
-function szures() {
-    var kategoria = document.getElementById('szures').value;
-    window.open("https://martakrisztian97.github.io/SweetAndSnacks/preorder.html#".kategoria);
-    document.getElementById('szuresBtn').text = kategoria;
-}
